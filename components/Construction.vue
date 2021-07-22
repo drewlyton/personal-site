23756e5b618dfef5cad142d2f98b0db1e5d90b16
Drew Lytle <aml3ed@virginia.edu>
2021-07-22 14:42:25 -0400

    build(Tailwind): Add tailwind config file

---
+++ components/Construction.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    class="
+      p-10
+      max-w-md
+      bg-white
+      rounded-xl
+      shadow-md
+      flex
+      items-center
+      justify-center
+      space-x-4
+    "
+  >
+    <h1 class="text-3xl">🔨 Under Construction</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Hello Friends</h1>
+    <construction />
   </div>
 </template>
 
