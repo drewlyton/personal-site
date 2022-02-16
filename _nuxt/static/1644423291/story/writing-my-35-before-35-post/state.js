7fcfe02bb83782e4cf9b471d4c59a90747a436cb
aml3ed <31733517+aml3ed@users.noreply.github.com>
2022-02-16 14:34:14 +0000

    Deploying to gh-pages from @ aml3ed/personal-site@4a62fc52a538e2b38339872aec8a586b1e221964 🚀

--- _nuxt/static/1644423291/story/writing-my-35-before-35-post/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644423291",layout:"default",error:a,serverRendered:true,routePath:"\u002Fstory\u002Fwriting-my-35-before-35-post",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
