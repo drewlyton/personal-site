71bacd9f86725927f6eedab9940852171894a6f6
aml3ed <31733517+aml3ed@users.noreply.github.com>
2022-02-23 15:18:33 +0000

    Deploying to gh-pages from @ aml3ed/personal-site@75661887c49ca56732a7f108b032f9d3f676dac1 🚀

--- _nuxt/static/1645022050/story/10-things-i-learned-from-my-first-failed-startup-post/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645022050",layout:"default",error:a,serverRendered:true,routePath:"\u002Fstory\u002F10-things-i-learned-from-my-first-failed-startup-post",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
